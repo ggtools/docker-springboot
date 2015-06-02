83510807d5f0d6850b5d129c9f00adc04ce38a2b
Christophe Labouisse <christophe@labouisse.org>
2015-06-02 10:39:11 +0200

    Add `${JAVA_OPTS}` to the `java` command

--- docker-entry-point.sh
+++ docker-entry-point.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ -z "$1" -o  "${1:0:1}" = '-' ]; then
     cd $HOME
-    exec gosu default java -Djava.security.egd=file:/dev/urandom -jar lib/$SB_APP_JAR --spring.profiles.active=${SB_ACTIVE_PROFILES} "$@"
+    exec gosu default java ${JAVA_OPTS} -Djava.security.egd=file:/dev/urandom -jar lib/$SB_APP_JAR --spring.profiles.active=${SB_ACTIVE_PROFILES} "$@"
 fi
 
 exec "$@"
